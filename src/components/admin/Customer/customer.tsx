7378ee9e1b1bff04ba30c3e2ca6b218bbe66c5c2
mk <minhkhanh04082004@gmail.com>
2025-04-15 09:48:52 +0700

    customer-build

--- src/components/admin/Customer/customer.tsx
+++ src/components/admin/Customer/customer.tsx
@@ -1,4 +1,5 @@
-import { useState, ChangeEvent } from 'react'
+import { useState } from 'react'
+import 'react-toastify/dist/ReactToastify.css'
 import {
   Table,
   TableBody,
@@ -11,53 +12,25 @@ import {
   Pagination,
   Stack,
   Box,
-  Chip
+  Snackbar
 } from '@mui/material'
 import { CiCirclePlus } from 'react-icons/ci'
 import { IoSearch } from 'react-icons/io5'
 import CustomerDialog from '@/components/admin/Customer/components/dialog'
 import { Customer } from '@/services/customer.service'
-import { formatDate } from '@/helpers'
 import { useCustomer } from '@/hooks/useCustomer'
 
 const CustomerPage = () => {
-  const [filters, setFilters] = useState<Record<string, string[]>>({})
-  const [searchTerm, setSearchTerm] = useState('')
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [selectedCustomer, setSelectedCustomer] = useState<Customer | null>(null)
   const [page, setPage] = useState(1)
   const limit = 10
   const accessToken = localStorage.getItem('accessToken') || ''
   const { data, isLoading, isError } = useCustomer({ accessToken, page, limit })
   const customers = data?.data.data || []
-  const totalPage = data?.data.totalPages || 1
-  const getRankColor = (rank: string) => {
-    switch (rank) {
-      case 'Bronze':
-        return { textColor: 'brown', bgColor: 'rgba(216, 155, 99, 0.8)' }
-      case 'Silver':
-        return { textColor: 'gray', bgColor: 'rgba(211, 211, 211, 0.8)' }
-      case 'Gold':
-        return { textColor: '#FF8000', bgColor: 'rgba(245, 224, 110, 0.8)' }
-      case 'Diamond':
-        return { textColor: '#0080FF', bgColor: 'rgba(224, 247, 255, 0.8)' }
-      default:
-        return { textColor: '#000', bgColor: 'rgba(204, 204, 204, 0.8)' }
-    }
-  }
-
-  const [dialogOpen, setDialogOpen] = useState(false)
-  const [selectedCustomer, setSelectedCustomer] = useState<Customer | null>(null)
-
-  const clearFilter = (column: string) => {
-    setFilters((prev) => {
-      const newFilters = { ...prev }
-      delete newFilters[column]
-      return newFilters
-    })
-  }
-
-  const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(event.target.value)
-  }
+  const totalPages = data?.data.totalPages || 1
+  const [snackbarOpen, setSnackbarOpen] = useState(false)
+  const [snackbarMessage, setSnackbarMessage] = useState('')
 
   const openDialogForAdd = () => {
     setSelectedCustomer(null)
@@ -73,30 +46,21 @@ const CustomerPage = () => {
     setDialogOpen(false)
   }
 
-  const handleSaveCustomer = (customer: Customer) => {
-    console.log('Save customer', customer)
+  const handleSnackbar = (message: string) => {
+    setSnackbarMessage(message)
+    setSnackbarOpen(true)
+  }
+
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false)
   }
 
   return (
     <div className='p-4 pt-8'>
-      {/* Header */}
       <Box display='flex' justifyContent='space-between' alignItems='center' mb={2}>
         <Button variant='contained' color='success' startIcon={<CiCirclePlus />} onClick={openDialogForAdd}>
           Thêm khách hàng
         </Button>
-        <Box>
-          {Object.entries(filters).map(([column, values]) =>
-            values.length > 0 ? (
-              <Chip
-                className='rounded-xl'
-                key={column}
-                label={`${values.join(', ')}`}
-                onDelete={() => clearFilter(column)}
-                style={{ marginRight: 8 }}
-              />
-            ) : null
-          )}
-        </Box>
       </Box>
       <div className='border-t border-gray-300 w-full'></div>
       <Box display='flex' justifyContent='space-between' alignItems='center' my={1}>
@@ -107,23 +71,18 @@ const CustomerPage = () => {
             type='text'
             placeholder='Search'
             className='text-black border-none outline-none w-full hidden md:block'
-            value={searchTerm}
-            onChange={handleSearchChange}
           />
         </div>
       </Box>
-      {/* Bảng hiển thị Customer */}
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label='customer table'>
           <TableHead>
             <TableRow>
-              <TableCell>Số thứ tự</TableCell>
+              <TableCell>#</TableCell>
               <TableCell>Tên khách hàng</TableCell>
               <TableCell>Số điện thoại</TableCell>
-              <TableCell>Ngày tham gia</TableCell>
-              <TableCell>Đơn hàng</TableCell>
-              <TableCell>Điểm</TableCell>
-              <TableCell>Hạng</TableCell>
+              <TableCell>Rank</TableCell>
+              <TableCell>Point</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -131,32 +90,29 @@ const CustomerPage = () => {
               customers.map((customer, index) => (
                 <TableRow
                   key={index}
-                  sx={{ backgroundColor: index % 2 === 0 ? 'action.hover' : 'inherit' }}
+                  sx={{
+                    backgroundColor: index % 2 === 0 ? 'action.hover' : 'inherit'
+                  }}
                   onClick={() => openDialogForEdit(customer)}
                 >
-                  <TableCell>{index}</TableCell>
-                  <TableCell>{customer.name}</TableCell>
-                  <TableCell>{customer.phone}</TableCell>
-                  <TableCell>{formatDate(customer.createdAt)}</TableCell>
-                  <TableCell>{2}</TableCell>
-                  <TableCell>{customer.customer.point}</TableCell>
+                  <TableCell>{(page - 1) * limit + index + 1}</TableCell>
                   <TableCell>
-                    <Chip
-                      label={customer.customer.rank}
-                      sx={{
-                        borderRadius: '4px',
-                        backgroundColor: getRankColor(customer.customer.rank).bgColor,
-                        color: getRankColor(customer.customer.rank).textColor,
-                        display: { xs: 'none', md: 'table-cell' }
-                      }}
-                    />
+                    <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+                      <div>
+                        {customer.user.name} <br />
+                        <span style={{ color: 'gray', fontSize: '0.875rem' }}>{customer.userId}</span>
+                      </div>
+                    </div>
                   </TableCell>
+                  <TableCell>{customer.user.phone}</TableCell>
+                  <TableCell>{customer.point}</TableCell>
+                  <TableCell>{customer.rank}</TableCell>
                 </TableRow>
               ))
             ) : (
               <TableRow>
-                <TableCell colSpan={7} style={{ textAlign: 'center' }}>
-                  {isLoading ? 'Loading...' : isError ? 'Error loading customers' : 'No customers found'}
+                <TableCell colSpan={6} align='center'>
+                  {isLoading ? 'Đang tải...' : isError ? 'Lỗi khi tải khách hàng' : 'Không tìm thấy khách hàng'}
                 </TableCell>
               </TableRow>
             )}
@@ -166,19 +122,26 @@ const CustomerPage = () => {
 
       <Stack spacing={2} sx={{ marginTop: 2, alignItems: 'center' }}>
         <Pagination
-          count={totalPage}
+          count={totalPages}
+          page={page}
+          onChange={(_, value) => setPage(value)}
           variant='outlined'
           shape='rounded'
-          onChange={(_e, value) => {
-            setPage(value)
-          }}
         />
       </Stack>
       <CustomerDialog
         open={dialogOpen}
         onClose={handleDialogClose}
-        onSave={handleSaveCustomer}
         customer={selectedCustomer}
+        accessToken={accessToken}
+        onActionSuccess={handleSnackbar}
+      />
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        open={snackbarOpen}
+        autoHideDuration={5000}
+        onClose={handleSnackbarClose}
+        message={snackbarMessage}
       />
     </div>
   )
